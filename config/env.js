e41a875fd4f306df8b93bfa7cbc5a88c1fe6a4be
Alya Zagatskaya <alya.zagacki@gmail.com>
2022-07-09 17:30:44 +0300

    test eslint

--- config/env.js
+++ config/env.js
@@ -8,7 +8,8 @@ const paths = require('./paths');
 delete require.cache[require.resolve('./paths')];
 
 const NODE_ENV = process.env.NODE_ENV;
-console.log(process.env);
+console.log(process.env.NODE_ENV);
+console.log(process.env.BABEL_ENV);
 if (!NODE_ENV) {
   throw new Error(
     'The NODE_ENV environment variable is required but was not specified.'
