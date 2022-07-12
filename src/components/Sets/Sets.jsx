4f97fc053a361b716c96370a4af8d07de32c9e4e
Alya Zagatskaya <alya.zagacki@gmail.com>
2022-07-12 18:27:02 +0300

    27_week_3

--- src/components/Sets/Sets.jsx
+++ src/components/Sets/Sets.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import styles from "./Sets.css";
 import CardSetButton from "../CardSetButton/CardSetButton.jsx";
-import CustomizedTables from "../SetsTable/SetsTable.jsx";
-import EnhancedTable from "../SetsTable/EnhancedTable.jsx";
-// import Card from "../Card/Card.jsx";
+import EnhancedTable from "../EnhancedTable/EnhancedTable.jsx";
+import Card from "../Card/Card.jsx";
 import words from "../../words.json";
+
+function handleSetButtonClick(e) {
+  console.log(e.target);
+  <Card item={words[0]}></Card>;
+}
 
 function Sets(props) {
   return (
     <div className={styles.sets__wrapper}>
       <div className={styles.sets__buttons}>
         {words.map((el) => (
-          <CardSetButton cardSet={el}></CardSetButton>
+          <CardSetButton
+            cardSet={el}
+            onClick={handleSetButtonClick}
+          ></CardSetButton>
         ))}
       </div>
       {/* <CustomizedTables></CustomizedTables> */}
       <EnhancedTable></EnhancedTable>
-      {/* {<Card item={words[0]}></Card>} */}
     </div>
   );
 }
